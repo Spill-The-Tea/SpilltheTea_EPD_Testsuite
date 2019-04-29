ef699ede21acf7f6b4474398d7023bbeb4fa88d8
Spill-The-Tea <44797905+Spill-The-Tea@users.noreply.github.com>
2019-04-29 04:22:47 -0700

    Update pgn2epd.py

--- Tools/pgn2epd.py
+++ Tools/pgn2epd.py
@@ -2,14 +2,13 @@
 
 ### A simple, yet useful Python Script to Convert a PGN File (Test Suites) to
 ### EPD compatible format, by taking the first move in the file as the Best
-### (or suggested) Move.
+### Move.
 ###
-### The FEN String is taken from the FEN Tags
-### Unfortunately, this does not take any other information associated
-### with positions, such as an ID, or Player Names, etc...
+### The FEN String is taken from the FEN Tags. Unfortunately, this does not 
+### take any other information associated with positions, such as an ID...
 ###
 ### Script taken from tools folder of the Giraffe Engine created by Matthew Lai
-### https://github.com/AFDudley/giraffe/tree/master/tools
+### https://bitbucket.org/waterreaction/giraffe/src/d3aa7af342db8b0b254f030d5ca5b674b5f83181/tools/pgn2epd.py?at=default&fileviewer=file-view-default
 
 import chess
 import sys
@@ -30,7 +29,7 @@ while game != None:
 	
 	gameNode = game
 	while len(gameNode.variations):
-		print(gameNode.board().epd(sm = gameNode.variations[0].move))
+		print(gameNode.board().epd(bm = gameNode.variations[0].move))
 		gameNode = gameNode.variations[0]
 	
 	game = chess.pgn.read_game(pgn)
